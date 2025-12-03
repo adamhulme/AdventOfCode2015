0c694cf4959e9cb9d76f4798073bcd214687e9fa
Adam Hulme <adamhulme4@gmail.com>
2025-12-03 17:29:56 +0000

    Add day 3/4

---
+++ day3/Program.cs
@@ -0,0 +1,59 @@
+﻿class Program
+{
+    public int zeroCount = 0;
+    static void Main(string[] args)
+    {
+        StreamReader sr = new StreamReader("C:\\projects\\aoc15\\day3\\in.txt");
+        var line = sr.ReadLine();
+        var deliveries = new HashSet<(int, int)>();
+        deliveries.Add((0, 0));
+        var santaVertical = 0;
+        var santaHorizontal = 0;
+        var robosantaVertical = 0;
+        var robosantaHorizontal = 0;
+        var turn = false;
+        while (line != null)
+        {
+            foreach (var c in line)
+            {
+                if (turn)
+                {
+                    if (c == '^')
+                        robosantaVertical++;
+                    else if (c == 'v')
+                        robosantaVertical--;
+                    else if (c == '>')
+                        robosantaHorizontal++;
+                    else if (c == '<')
+                        robosantaHorizontal--;
+
+                    var tuple = (robosantaHorizontal, robosantaVertical);
+                    Console.WriteLine($"Delivering to: {tuple}");
+                    deliveries.Add(tuple);
+                    turn = false;
+                    continue;
+                }
+                else 
+                {
+                    if (c == '^')
+                        santaVertical++;
+                    else if (c == 'v')
+                        santaVertical--;
+                    else if (c == '>')
+                        santaHorizontal++;
+                    else if (c == '<')
+                        santaHorizontal--;
+                        
+                    var tuple = (santaHorizontal, santaVertical);
+                    Console.WriteLine($"Delivering to: {tuple}");
+                    deliveries.Add(tuple);
+                }
+                turn = !turn;
+            }
+            line = sr.ReadLine();
+        }
+        Console.WriteLine(deliveries.Count);
+        sr.Close();
+        Console.ReadLine();
+    }
+}
---
+++ day4/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    public int zeroCount = 0;
+    static void Main(string[] args)
+    {
+        var input = "iwrupvqb";
+        var found = false;
+        for (int i = 0; !found; i++)
+        {
+            var toHash = input + i;
+            using var md5 = System.Security.Cryptography.MD5.Create();
+            var inputBytes = System.Text.Encoding.ASCII.GetBytes(toHash);
+            var hashBytes = md5.ComputeHash(inputBytes);
+            var hashString = BitConverter.ToString(hashBytes).Replace("-", "").ToLowerInvariant();
+            if (hashString.StartsWith("000000"))
+            {
+                Console.WriteLine($"Found: {i}");
+                found = true;
+            }
+        }
+        Console.ReadLine();
+    }
+}

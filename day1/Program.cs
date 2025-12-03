812e90679f6c1d39513244171d6f16aa75975742
Adam Hulme <adamhulme4@gmail.com>
2025-12-03 16:59:29 +0000

    Add day 1 sln

---
+++ day1/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    public int zeroCount = 0;
+    static void Main(string[] args)
+    {
+        StreamReader sr = new StreamReader("C:\\projects\\aoc15\\day1\\in.txt");
+        var line = sr.ReadLine();
+        var floor = 0;
+        while (line != null)
+        {
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == '(')
+                    floor++;
+                else if (line[i] == ')')
+                    floor--;
+
+                if (floor == -1)
+                {
+                    Console.WriteLine($"Basement entered: {i + 1}");
+                }
+            }
+            line = sr.ReadLine();
+        }
+        sr.Close();
+        Console.WriteLine($"Final floor: {floor}");
+        Console.ReadLine();
+    }
+}
